38ec5746ed09fe90f2c1947cc473c6b51c94ce2d
André Lashley <andre.lashley@gmail.com>
2022-05-07 18:44:50 -0700

    Modify house hook to use react query for caching and re-fetching data

--- reactweb/src/hooks/HouseHooks.ts
+++ reactweb/src/hooks/HouseHooks.ts
@@ -1,20 +1,13 @@
-import { useEffect, useState } from 'react';
+import axios, { AxiosError } from 'axios';
+import { useQuery } from 'react-query';
 import config from '../config';
 import { House } from '../types/house';
 
-const useFetchHouses = (): House[] => {
-    const [houses, setHouses] = useState<House[]>([]);
-
-    useEffect(() => {
-        const fetchHouses = async () => {
-            const rsp = await fetch(`${config.baseApiUrl}/houses`);
-            const houses = await rsp.json();
-            setHouses(houses);
-        };
-        fetchHouses();
-    }, []);
-
-    return houses;
+const useFetchHouses = () => {
+    return useQuery<House[], AxiosError>("houses", () =>
+        axios.get(`${config.baseApiUrl}/houses`).then(
+            (resp) => resp.data)
+    );
 }
 
 export default useFetchHouses;
