c5fcdcf8286bec19cd0533d115ad2479b6d6a8f4
André Lashley <andre.lashley@gmail.com>
2022-05-08 17:10:03 -0700

    Add mutations to react app and invalidate cache where necessary

--- reactweb/src/hooks/HouseHooks.ts
+++ reactweb/src/hooks/HouseHooks.ts
@@ -1,5 +1,6 @@
-import axios, { AxiosError } from 'axios';
-import { useQuery } from 'react-query';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { useNavigate } from 'react-router-dom';
 import config from '../config';
 import { House } from '../types/house';
 
@@ -8,12 +9,54 @@ const useFetchHouses = () => {
         axios.get(`${config.baseApiUrl}/houses`).then(
             (resp) => resp.data)
     );
-}
+};
 
 const useFetchHouse = (id: number) => {
     return useQuery<House, AxiosError>(["houses", id], () =>
         axios.get(`${config.baseApiUrl}/house/${id}`).then((resp) => resp.data));
-}
+};
+
+const useAddHouse = () => {
+    const nav = useNavigate();
+    const queryClient = useQueryClient();
+    return useMutation<AxiosResponse, AxiosError, House>(
+        (h) => axios.post(`${config.baseApiUrl}/houses`, h),
+        {
+            onSuccess: () => {
+                queryClient.invalidateQueries("houses");
+                nav("/");
+            }
+        }
+    );
+};
+
+const useUpdateHouse = () => {
+    const nav = useNavigate();
+    const queryClient = useQueryClient();
+    return useMutation<AxiosResponse, AxiosError, House>(
+        (h) => axios.put(`${config.baseApiUrl}/houses`, h),
+        {
+            onSuccess: (_, house) => {
+                queryClient.invalidateQueries("houses");
+                nav(`/house/${house.id}`);
+            }
+        }
+    );
+};
+
+const useDeleteHouse = () => {
+    const nav = useNavigate();
+    const queryClient = useQueryClient();
+    return useMutation<AxiosResponse, AxiosError, House>(
+        (h) => axios.delete(`${config.baseApiUrl}/houses/${h.id}`),
+        {
+            onSuccess: (_, house) => {
+                queryClient.invalidateQueries("houses");
+                nav(`/house/${house.id}`);
+            }
+        }
+    );
+};
 
 export default useFetchHouses;
-export { useFetchHouse };
+export { useFetchHouse, useAddHouse, useUpdateHouse, useDeleteHouse };
